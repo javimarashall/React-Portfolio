f6bc65d78a56dbc972b27c6164cba5d374cc74c1
javimarashall <jamondragon@ucdavis.edu>
2021-05-22 10:18:36 -0700

    footer

---
+++ portfolioapp/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+
+const Footer = () => {
+
+return (
+    <nav class="navbar fixed-bottom navbar-light bg-light">
+        <a class="navbar-brand" href="#">Fixed bottom</a>
+    </nav>
+)
+}
+
+export default Footer;
